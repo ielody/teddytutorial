a17880704e1357e4a474fd9ef80179c8a02c59a0
Ingeborg Eldøy <ieldoy@gmail.com>
2023-03-20 13:19:31 +0100

    for loops

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // let rat = "radioactive subway rat"
 // console.log("yesterday in the subway in NYC" + " " + rat)
 
+//OBJECT TEST
 const animalFarm = {
   name: "Lotta the pig",
   age: 8,
@@ -33,3 +34,47 @@ console.log(animalFarm)
 //Nested objects, object within an object
 console.log(animalFarm.nickName[1])
 console.log(animalFarm.metaData.picture)
+
+
+
+//Stack and heap explained (Value & reference type)
+//primitive type(small data type): string, boolean, number
+//primitive types goes to stack
+//reference type goes to heap, bigger types as object and array
+
+
+//for loop
+
+let turtleString = "turtle"
+for(let i = 0; i < turtleString.length; i++) {
+  console.log(turtleString[i])
+}
+
+//CREATE
+let birds = ['hummingbird', 'magpie', 'crow', 'sparrow']
+
+for(i = 0; i < birds.length; i++) {
+  console.log(birds[i])
+}
+
+const bullFrogs = [
+  {
+    name: "great banjo toad",
+    location: "australia",
+    isThreatened: false
+  },
+  {
+    name: "american bullfrog",
+    location: "america",
+    isThreatened: false,
+  },
+  {
+    name: "toadman",
+    location: "android galaxy",
+    isThreatened: true,
+  }
+]
+
+for(let i = 0; i < bullFrogs.length; i++) {
+  console.log(`${bullFrogs[i].name} is located in ${bullFrogs[i].location} and is ${bullFrogs[i].isThreatened}`)
+}
